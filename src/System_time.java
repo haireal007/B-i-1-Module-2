32047fbd594c2328801209f163f1f3cd90fce36b
unknown <hairealmta@gmail.com>
2022-05-09 11:07:07 +0700

    1

---
+++ src/System_time.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class System_time {
+    public static void main(String[] args){
+        Date now = new Date();
+        System.out.println("Now is" +" "+ now);
+    }
+}
